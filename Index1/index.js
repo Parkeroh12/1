0622cc7d39734d30c850cb0905fb6775209b7177
Parkeroh12 <parkerohayley1@gmail.com>
2022-04-24 23:14:20 -0700

    updating it

---
+++ Index1/index.js
@@ -0,0 +1,17 @@
+/*let header = document.querySelector('h2')
+
+for(let i = 0; i < 1 ;i++){
+    header.style.border = 'solid black 5px'}
+header.addEventListener('click', function(e){
+    
+})
+let images = document.querySelector("img")
+    
+images.style.border = 'solid black 5px'
+images.style.borderRadius = '1000px'*/
+
+
+
+
+
+
